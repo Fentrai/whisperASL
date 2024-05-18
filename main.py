4021ce59168b52783e990e399f2abce632d4a31f
unknown <Ishan Soni>
2024-05-18 14:00:37 -0400

    With functions and working speech

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from PIL import Image
+# import matplotlib.pyplot as plt
 import whisper
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
 model = whisper.load_model("base")
 
 result = model.transcribe("hellothere.mp3")
-print(result["text"])
+transcribedText = result["text"]
+print(transcribedText)
 
+
+img = mpimg.imread('aslImages/0.png')
+plt.imshow(img)
+plt.show()
