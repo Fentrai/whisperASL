f683fcaf6ee7234f06d8800589296950d56447c0
unknown <Ishan Soni>
2024-05-18 14:08:35 -0400

    finished for loops plz work

--- main.py
+++ main.py
@@ -3,14 +3,62 @@ from PIL import Image
 import whisper
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import string
 
 model = whisper.load_model("base")
+alph = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+def get_indices(word_list):
+    indices_list = []
+
+    for word in word_list:
+        indices = []
+        for letter in word:
+            if letter.isalpha():
+                indices.append(alph.index(letter))
+        indices_list.append(indices)
+
+    return indices_list
+
+
+def clean_string(input_string):
+    # Create a list of words using the input string
+    word_list = input_string.split()
+
+    # Remove punctuation from each word
+    word_list = [word.translate(str.maketrans('', '', string.punctuation)) for word in word_list]
+
+    # Convert all capital letters to lowercase
+    word_list = [word.lower() for word in word_list]
+
+    return word_list
+
+
 
 result = model.transcribe("hellothere.mp3")
 transcribedText = result["text"]
 print(transcribedText)
+words = clean_string(transcribedText)
+numsListToDisplay = get_indices(words)
+
+for i in numsListToDisplay:
+    letter = mpimg.imread('aslImages/' + i + '.png')
+    plt.imshow(letter)
+    plt.show()
+#create list of words using split
+
+#for loop for each word
+    #lowercase
+    #for loop for each character
+        #get index of character in alph
+        #if the index isnt -1 then display the image
+ 
+#clean data: capital letters, punctuation 
 
 
-img = mpimg.imread('aslImages/0.png')
-plt.imshow(img)
-plt.show()
+
+
+# img = mpimg.imread('aslImages/0.png')
+# plt.imshow(img)
+# plt.show()
+
